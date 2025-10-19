e13d39ce48ec95c976277d1295ed7c5286b784b8
SpaceXEngineer <148818352+FalconDevX@users.noreply.github.com>
2025-10-19 20:01:23 +0200

    Add Gauss integration functionality and .gitignore file

---
+++ gauss_integrations.py
@@ -0,0 +1,11 @@
+from gauss_schemes import GaussIntegral
+
+def function_1d(x : float) -> float:
+    return 5*(x**2) + 3*x + 6
+
+def function_2d(x : float, y : float) -> float:
+    return 5*(x**2)*(y**2) + 3*x*y + 6
+
+integral_1d = GaussIntegral(2, function_1d, 1)
+
+print(integral_1d.integrate())
